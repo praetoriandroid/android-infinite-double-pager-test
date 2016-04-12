14678ba385f2554a3ef942313b4ec128910a249d
Victor Demchenko <praetoriandroid@gmail.com>
2016-04-12 03:42:17 +0300

    Move all magic to separate class

--- app/src/main/java/com/example/infinitedoublepager/InfinitePagerActivity.java
+++ app/src/main/java/com/example/infinitedoublepager/InfinitePagerActivity.java
@@ -5,25 +5,14 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.FrameLayout;
 import android.widget.TextView;
 
-import static android.support.v4.view.ViewPager.SCROLL_STATE_IDLE;
-
 public class InfinitePagerActivity extends AppCompatActivity {
-
-    private static final int NOT_SET = -1;
-
-    private ViewGroup[] pageContainers;
-    private View primaryPage;
-    private View secondaryPage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,113 +20,12 @@ public class InfinitePagerActivity extends AppCompatActivity {
         ViewPager pager = new ViewPager(this);
         setContentView(pager);
 
-        pageContainers = new ViewGroup[]{
-                new FrameLayout(this),
-                new FrameLayout(this),
-                new FrameLayout(this),
-                new FrameLayout(this)
-        };
-        pager.setAdapter(new PagerAdapter() {
-
-            @Override
-            public int getCount() {
-                return Integer.MAX_VALUE;
-            }
-
-            @Override
-            public boolean isViewFromObject(View view, Object object) {
-                return view == object;
-            }
-
-            @Override
-            public Object instantiateItem(ViewGroup container, int position) {
-                Log.d("###", "instantiateItem(container, " + position + ")");
-                ViewGroup item = pageContainers[position % 4];
-                container.addView(item);
-                return item;
-            }
-
-            @Override
-            public void destroyItem(ViewGroup container, int position, Object object) {
-                Log.d("###", "destroyItem(container, " + position + ")");
-                container.removeView(pageContainers[position % 4]);
-            }
-        });
-        final int initialPosition = Integer.MAX_VALUE / 2 - (Integer.MAX_VALUE / 2) % 4;
-        pager.setCurrentItem(initialPosition);
-
-        primaryPage = new PageContentView(this, 0xff6060ff, "even");
+        View primaryPage = new PageContentView(this, 0xff6060ff, "even");
         Log.d("###", "even: " + primaryPage);
-        secondaryPage = new PageContentView(this, 0xffff6060, "odd");
+        View secondaryPage = new PageContentView(this, 0xffff6060, "odd");
         Log.d("###", "odd: " + secondaryPage);
-        pageContainers[0].addView(primaryPage);
-        pager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            int primaryPosition = initialPosition;
-            int secondaryPosition = NOT_SET;
-            int selectedPosition = NOT_SET;
-
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-                Log.d("###", "onPageScrolled(" + position + ", " + positionOffset + ", " + positionOffsetPixels + ")");
-                if (positionOffsetPixels == 0) {
-                    selectedPosition = position;
-                    return;
-                }
-
-                int secondaryPageOffset = position == primaryPosition ? 1 : -1;
-                int currentSecondaryPosition = primaryPosition + secondaryPageOffset;
-                if (secondaryPosition != currentSecondaryPosition) {
-                    if (secondaryPosition != NOT_SET) {
-                        removePage(secondaryPosition, secondaryPage);
-                    }
-                    addPage(currentSecondaryPosition, secondaryPage);
-                    secondaryPosition = currentSecondaryPosition;
-                }
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-                Log.d("###", "onPageSelected(" + position + ")");
-                selectedPosition = position;
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-                Log.d("###", "onPageScrollStateChanged(" + state + ")");
-                switch (state) {
-                    case SCROLL_STATE_IDLE:
-//                        Log.d("###", "stop scrolling (to the " + (scrollToLeftPage ? "left" : "right") + " page)");
-                        boolean pageSelectionChanged = primaryPosition != selectedPosition;
-                        Log.d("###", "stop scrolling: page" + (pageSelectionChanged ? "" : " not") + " changed, primary: " + primaryPosition + "[" + primaryPage + "], secondary: " + secondaryPosition + "[" + secondaryPage + "], selected: " + selectedPosition);
-                        if (pageSelectionChanged) {
-                            swapPrimaryPage();
-                            secondaryPosition = primaryPosition;
-                            primaryPosition = selectedPosition;
-                        }
-                        if (secondaryPosition != NOT_SET) {
-                            removePage(secondaryPosition, secondaryPage);
-                        }
-                        secondaryPosition = NOT_SET;
-                        break;
-                }
-            }
-
-            private void removePage(int position, View page) {
-                Log.d("###", "removePage(" + position + ", " + page + ")");
-                pageContainers[position % 4].removeView(page);
-            }
-
-            private void addPage(int position, View page) {
-                Log.d("###", "addPage(" + position + ", " + page + ")");
-                pageContainers[position % 4].addView(page);
-            }
-
-            private void swapPrimaryPage() {
-                View page = primaryPage;
-                primaryPage = secondaryPage;
-                secondaryPage = page;
-            }
-        });
+        TwoPageCarousel carousel = new TwoPageCarousel(this, primaryPage, secondaryPage);
+        carousel.setupPager(pager);
     }
 
     @SuppressLint("ViewConstructor")
---
+++ app/src/main/java/com/example/infinitedoublepager/TwoPageCarousel.java
@@ -0,0 +1,139 @@
+package com.example.infinitedoublepager;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import static android.support.v4.view.ViewPager.SCROLL_STATE_IDLE;
+
+public class TwoPageCarousel {
+
+    private static final int NOT_SET = -1;
+    private static final int INITIAL_POSITION = Integer.MAX_VALUE / 2 - (Integer.MAX_VALUE / 2) % 4;
+
+    private ViewGroup[] pageContainers;
+    private View primaryPage;
+    private View secondaryPage;
+
+    public TwoPageCarousel(Context context, View primaryPage, View secondaryPage) {
+        this.primaryPage = primaryPage;
+        this.secondaryPage = secondaryPage;
+        pageContainers = new ViewGroup[]{
+                new FrameLayout(context),
+                new FrameLayout(context),
+                new FrameLayout(context),
+                new FrameLayout(context)
+        };
+        pageContainers[0].addView(primaryPage);
+    }
+
+    public void setupPager(ViewPager pager) {
+        pager.setAdapter(new CarouselPagerAdapter());
+        pager.addOnPageChangeListener(new PageJuggler());
+        pager.setCurrentItem(INITIAL_POSITION);
+    }
+
+    private class CarouselPagerAdapter extends PagerAdapter {
+        @Override
+        public int getCount() {
+            return Integer.MAX_VALUE;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            Log.d("###", "instantiateItem(container, " + position + ")");
+            ViewGroup item = pageContainers[position % 4];
+            container.addView(item);
+            return item;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            Log.d("###", "destroyItem(container, " + position + ")");
+            container.removeView(pageContainers[position % 4]);
+        }
+    }
+
+    private class PageJuggler implements ViewPager.OnPageChangeListener {
+        int primaryPosition = INITIAL_POSITION;
+        int secondaryPosition = NOT_SET;
+        int selectedPosition = NOT_SET;
+
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            if (positionOffsetPixels == 0) {
+                Log.d("###", "onPageScrolled(" + position + ", " + positionOffset + ", " + positionOffsetPixels + "): select position: " + position);
+                selectedPosition = position;
+                return;
+            }
+
+            int secondaryPageOffset = position == primaryPosition ? 1 : -1;
+            int currentSecondaryPosition = primaryPosition + secondaryPageOffset;
+            if (secondaryPosition != currentSecondaryPosition) {
+                Log.d("###", "onPageScrolled(" + position + ", " + positionOffset + ", " + positionOffsetPixels + "): secondary position changed: " + secondaryPosition + " -> " + currentSecondaryPosition);
+                if (secondaryPosition != NOT_SET) {
+                    removePage(secondaryPosition, secondaryPage);
+                }
+                addPage(currentSecondaryPosition, secondaryPage);
+                secondaryPosition = currentSecondaryPosition;
+            }
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            Log.d("###", "onPageSelected(" + position + ")");
+            selectedPosition = position;
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+            Log.d("###", "onPageScrollStateChanged(" + state + ")");
+            switch (state) {
+                case SCROLL_STATE_IDLE:
+                    boolean pageSelectionChanged = primaryPosition != selectedPosition;
+                    Log.d("###", "stop scrolling: page" + (pageSelectionChanged ? "" : " not") + " changed, primary: " + primaryPosition + "[" + primaryPage + "], secondary: " + secondaryPosition + "[" + secondaryPage + "], selected: " + selectedPosition);
+                    if (pageSelectionChanged) {
+                        swapPrimaryPage();
+                        secondaryPosition = primaryPosition;
+                        primaryPosition = selectedPosition;
+                    }
+                    if (secondaryPosition != NOT_SET) {
+                        removePage(secondaryPosition, secondaryPage);
+                    }
+                    secondaryPosition = NOT_SET;
+                    break;
+            }
+        }
+
+        private void removePage(int position, View page) {
+            Log.d("###", "removePage(" + position + ", " + page + ")");
+            if (pageContainers[position % 4].getChildCount() == 0) {
+                throw new IllegalStateException();
+            }
+            if (pageContainers[position % 4].getChildAt(0) != page) {
+                throw new IllegalStateException();
+            }
+            pageContainers[position % 4].removeView(page);
+        }
+
+        private void addPage(int position, View page) {
+            Log.d("###", "addPage(" + position + ", " + page + ")");
+            pageContainers[position % 4].addView(page);
+        }
+
+        private void swapPrimaryPage() {
+            View page = primaryPage;
+            primaryPage = secondaryPage;
+            secondaryPage = page;
+        }
+    }
+}
